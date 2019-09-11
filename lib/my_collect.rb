e36a882eaf5d7d91e3f08255d4f2f35a842d281e
Blythe Pollard <blytheepollard@gmail.com>
2019-09-11 18:55:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-  
+  array[i].split(" ").first
   name = yield(array[i]).split(" ").first
   collection << name
   i += 1
